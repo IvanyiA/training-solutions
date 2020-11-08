22ea0916ea9bd16c32a49576d5b5050d07c04103
IvanyiA <trachemys13@gmail.com>
2020-11-08 09:54:55 +0100

    DivByThree commit

---
+++ src/main/java/statements/DivByThree.java
@@ -0,0 +1,13 @@
+package statements;
+
+import java.util.Scanner;
+
+public class DivByThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy egész számot: ");
+        int szam = scanner.nextInt();
+
+        System.out.println(szam % 3 == 0 ? "oszthtató hárommal" : "nem osztható hárommal");
+    }
+}
