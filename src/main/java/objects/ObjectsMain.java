dfac2ec590527df06fc984a0c086c86194f64cac
IvanyiA <trachemys13@gmail.com>
2020-11-14 21:08:31 +0100

    példányosítás tömbben és listában

--- src/main/java/objects/ObjectsMain.java
+++ src/main/java/objects/ObjectsMain.java
@@ -1,5 +1,9 @@
 package objects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ObjectsMain {
 
     public static void main(String[] args) {
@@ -32,5 +36,13 @@ public class ObjectsMain {
 
         System.out.println(anotherBook instanceof Book);
 
+        Book[] books = {new Book(), new Book(), new Book()};
+
+        List<Book> books1 = Arrays.asList(new Book(), new Book(), new Book());
+
+        List<Book> books2 = new ArrayList<>();
+        books2.add(new Book());
+        books2.add(new Book());
+        books2.add(new Book());
     }
 }
