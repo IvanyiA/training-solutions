22de091c4484b8c9deea97d1ef4ca987499b3804
IvanyiA <trachemys13@gmail.com>
2020-11-15 18:45:06 +0100

    tesztelés assertEquals-szal

---
+++ src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,17 @@
+package introjunit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GentlemanTest {
+
+    @Test
+    public void testByNormalName() {
+        String name = "John Doe";
+
+        String üdvözlés = new Gentleman().sayHello(name);
+
+        assertEquals("Hello John Doe", üdvözlés);
+    }
+}
