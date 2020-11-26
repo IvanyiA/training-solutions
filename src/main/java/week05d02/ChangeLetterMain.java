f4e3586bdf8abf3cc087b74240c73134634485e0
IvanyiA <trachemys13@gmail.com>
2020-11-26 12:21:07 +0100

    commit

---
+++ src/main/java/week05d02/ChangeLetterMain.java
@@ -0,0 +1,11 @@
+package week05d02;
+
+public class ChangeLetterMain {
+
+    public static void main(String[] args) {
+        ChangeLetter cl = new ChangeLetter();
+
+        String result = cl.changeVowels2("arviturotukorfurogep");
+        System.out.println(result);
+    }
+}
