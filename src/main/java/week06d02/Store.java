52411d7acdb029df79d23cbe075fc02b00de021b
IvanyiA <trachemys13@gmail.com>
2020-12-02 10:15:54 +0100

    Store

---
+++ src/main/java/week06d02/Store.java
@@ -0,0 +1,27 @@
+package week06d02;
+
+import java.util.List;
+
+public class Store {
+
+    private List<Product> products;
+
+    public Store(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public int getProductByCategoryName(Category categoryName) {
+        int count = 0;
+        for (Product product : products) {
+            if (product.getCategory() == categoryName) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
