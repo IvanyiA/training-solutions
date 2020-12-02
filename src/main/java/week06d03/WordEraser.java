dc93452489235a608feca2148f5c70c2bae2796d
IvanyiA <trachemys13@gmail.com>
2020-12-02 18:37:33 +0100

    WordEraser

---
+++ src/main/java/week06d03/WordEraser.java
@@ -0,0 +1,32 @@
+package week06d03;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class WordEraser {
+
+    public String eraseWord(String words, String word) {
+        String[] arr = words.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String item : arr) {
+            if (!item.equals(word)) {
+                sb.append(item).append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public String eraseWordWithScanner(String words, String word) {
+        Scanner scanner = new Scanner(words);
+        StringBuilder result = new StringBuilder();
+
+        while (scanner.hasNext()) {
+            String s = scanner.next();
+            if (!word.equals(s)) {
+                result.append(s).append(" ");
+            }
+        }
+        return result.toString().trim();
+    }
+
+}
