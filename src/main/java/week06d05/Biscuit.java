7cee58051b1faac02b7fe99d9c98bc9d6efb63f1
IvanyiA <trachemys13@gmail.com>
2020-12-06 21:36:28 +0100

    Biscuit

---
+++ src/main/java/week06d05/Biscuit.java
@@ -0,0 +1,22 @@
+package week06d05;
+
+public class Biscuit {
+
+    private BiscuitType type;
+    private int gramAmount;
+
+    public Biscuit(BiscuitType type, int gramAmount) {
+        this.type = type;
+        this.gramAmount = gramAmount;
+    }
+
+    public static Biscuit of(BiscuitType type, int gramAmount) {
+        return new Biscuit(type, gramAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "Type: " + type + ", amount: " + gramAmount + "g";
+    }
+}
+
