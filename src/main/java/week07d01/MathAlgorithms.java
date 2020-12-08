81392f48cc53661562308420d9122adfcc265291
IvanyiA <trachemys13@gmail.com>
2020-12-08 18:48:41 +0100

    MathAlgorithms

---
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -0,0 +1,25 @@
+package week07d01;
+
+public class MathAlgorithms {
+
+    public static boolean isPrime(int x) {
+
+        if (x <= 0) {
+            throw new IllegalArgumentException("Ez nem pozitív szám.");
+        }
+        boolean prime = true;
+        if (x == 1)
+            prime = false;
+
+        if (x >= 2) {
+            for (int i = 2; i <= Math.sqrt(x); i++) {
+                if (x % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        return prime;
+    }
+
+}
