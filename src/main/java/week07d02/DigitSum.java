0740e79c65df32b031be12bcdb8f1665ec79e252
IvanyiA <trachemys13@gmail.com>
2020-12-08 18:11:31 +0100

    DigitSum

--- src/main/java/week07d02/DigitSum.java
+++ src/main/java/week07d02/DigitSum.java
@@ -2,8 +2,6 @@ package week07d02;
 
 public class DigitSum {
 
-    private int x;
-
     public static int sumOfDigits(int x) {
         int sum = 0;
         while (x > 10) {
@@ -12,6 +10,15 @@ public class DigitSum {
             x = x / 10;
         }
         return x + sum;
+    }
+
+    public static int sumOfDigitsWithString(int num) {
+        int result = 0;
+        String digits = String.valueOf(num);
+        for (int i = 0; i < digits.length(); i++) {
+            result = result + digits.charAt(i);
+        }
+        return result;
     }
 
 
