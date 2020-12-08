e224b70422d7c93a7533b4c763cd161a0cae48ae
IvanyiA <trachemys13@gmail.com>
2020-12-08 13:33:07 +0100

    DigitSum

---
+++ src/main/java/week0702/DigitSum.java
@@ -0,0 +1,19 @@
+package week0702;
+
+public class DigitSum {
+
+    private int x;
+
+    public static int sumOfDigits(int x) {
+        int sum = 0;
+        while (x > 10) {
+            int reminder = x % 10;
+            sum = sum + reminder;
+            x = x / 10;
+
+        }
+        return x+ sum;
+
+    }
+
+}
