89e72968f15136114c18a4f68c924399e3a6cbf7
IvanyiA <trachemys13@gmail.com>
2020-12-12 20:53:46 +0100

    Car

---
+++ src/main/java/inheritanceconstructor/cars/Car.java
@@ -0,0 +1,50 @@
+package inheritanceconstructor.cars;
+
+public class Car {
+
+    private double fuelRate;
+    private double fuel;
+    private double tankCapacity;
+
+    public Car(double fuelRate, double fuel, double tankCapacity) {
+        if (fuel > tankCapacity) {
+            throw new IllegalArgumentException("Tank capacity is less than fuel!");
+        }
+        this.fuelRate = fuelRate;
+        this.fuel = fuel;
+        this.tankCapacity = tankCapacity;
+    }
+
+    public double getFuelRate() {
+        return fuelRate;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void modifyFuelAmount(double fuel) {
+        this.fuel += fuel;
+    }
+
+    public double getTankCapacity() {
+        return tankCapacity;
+    }
+
+    private boolean isEnoughFuel(int distance) {
+        return (fuel - distance * fuelRate / 100) >= 0.0;
+    }
+
+    // csökkenti az üzemanyag mennyiségét, nem fogyhat ki!
+    public void drive(int km) {
+        if (!isEnoughFuel(km)) {
+            throw new IllegalArgumentException("Not enough fuel available!");
+        }
+        fuel -= km * fuelRate / 100;
+    }
+
+    //kiszámolja, mennyit lehet tankolni
+    public double calculateRefillAmount() {
+        return tankCapacity - fuel;
+    }
+}
