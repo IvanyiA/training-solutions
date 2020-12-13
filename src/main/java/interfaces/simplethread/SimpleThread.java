bf08c53f29b217fa48660f59b2992950088ca166
IvanyiA <trachemys13@gmail.com>
2020-12-13 20:16:12 +0100

    SimpleThread

---
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -0,0 +1,33 @@
+package interfaces.simplethread;
+
+import java.util.List;
+
+public class SimpleThread implements Runnable {
+
+    private List<String> tasks;
+
+    public SimpleThread(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+
+    private boolean nextStep() {
+        if (tasks != null && !tasks.isEmpty()) {
+            tasks.remove(tasks.size() - 1);
+        }
+        return false;
+    }
+
+
+    @Override
+    public void run() {
+        while (!tasks.isEmpty()) {
+            nextStep();
+        }
+
+    }
+}
