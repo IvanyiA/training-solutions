c0199e98f23f27791bce1fcc7f9c6e94298d1ba7
IvanyiA <trachemys13@gmail.com>
2020-12-13 19:45:23 +0100

    Zoo

---
+++ src/main/java/interfaces/animal/Zoo.java
@@ -0,0 +1,27 @@
+package interfaces.animal;
+
+import java.util.List;
+
+public class Zoo {
+
+    private List<Animal> animals;
+
+    public Zoo(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public int getNumberOfLegs() {
+        int legs = 0;
+        for (Animal animal : animals) {
+            legs = legs + animal.getNumberOfLegs();
+        }
+        return legs;
+    }
+
+
+    public int getNumberOfAnimals() {
+        return animals.size();
+    }
+
+
+}
