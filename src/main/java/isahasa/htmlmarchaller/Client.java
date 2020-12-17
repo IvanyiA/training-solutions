2d9e65c4a7b58ad33ca8ec67fe9b79d784d3fecf
IvanyiA <trachemys13@gmail.com>
2020-12-17 10:52:01 +0100

    isa vs hasa

---
+++ src/main/java/isahasa/htmlmarchaller/Client.java
@@ -0,0 +1,16 @@
+package isahasa.htmlmarchaller;
+
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+
+    private Channel channel;
+
+    public Client(Channel channel) {
+        this.channel = channel;
+    }
+
+    public int writeToChannel(TextSource data) {
+        return channel.writeByte(data.getPlainText().getBytes());
+    }
+}
