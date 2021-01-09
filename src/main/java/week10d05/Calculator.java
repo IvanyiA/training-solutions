ccb0cf1625c08049e77ee7025adc1404aeab378e
IvanyiA <trachemys13@gmail.com>
2021-01-09 17:28:01 +0100

    Calculator

---
+++ src/main/java/week10d05/Calculator.java
@@ -0,0 +1,22 @@
+package week10d05;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+    public int findMinSum(int[] arr) {
+        if (arr.length < 4) {
+            throw new IllegalArgumentException("A listának legalább 4 eleműnek kell lennie");
+        }
+        Arrays.sort(arr);
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++) {
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+
+
+}
