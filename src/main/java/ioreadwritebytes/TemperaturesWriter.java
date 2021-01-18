9a0c4b48f2efad31e316e336743e49d51868328c
IvanyiA <trachemys13@gmail.com>
2021-01-18 12:53:58 +0100

    bytok írása write() olvasása readAllBytes()

---
+++ src/main/java/ioreadwritebytes/TemperaturesWriter.java
@@ -0,0 +1,19 @@
+package ioreadwritebytes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TemperaturesWriter {
+
+    public void writeTemperatures(Temperatures temperatures, String pathString) {
+
+        try {
+            Files.write(Path.of(pathString), temperatures.getData());
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("File not found: " + pathString, ioe);
+        }
+    }
+
+}
