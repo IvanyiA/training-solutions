c6d704b768d293cbf28520d9e9056302dd1fc5e4
IvanyiA <trachemys13@gmail.com>
2021-02-13 11:49:32 +0100

    kopikonstruktor létrehozása immutable LocalDate attributumon toLocalTime()

---
+++ src/main/java/clone/timesheetitem/TimeSheetItem.java
@@ -0,0 +1,54 @@
+package clone.timesheetitem;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TimeSheetItem {
+
+    private String employee;
+    private String project;
+    private LocalDateTime from;
+    private LocalDateTime to;
+
+
+    public TimeSheetItem(String employee, String project, LocalDateTime from, LocalDateTime to) {
+        this.employee = employee;
+        this.project = project;
+        this.from = from;
+        this.to = to;
+    }
+
+    public TimeSheetItem(TimeSheetItem timeSheetItem) {
+        employee = timeSheetItem.employee;
+        project = timeSheetItem.project;
+        from = timeSheetItem.from;
+        to = timeSheetItem.to;
+    }
+
+    public static TimeSheetItem withDifferentDay(TimeSheetItem timeSheetItem, LocalDate localDate) {
+
+        TimeSheetItem newtimeSheetItem = new TimeSheetItem(timeSheetItem);   //kopikonstruktor a példány adatait átmásolja az új példányba
+
+        newtimeSheetItem.from = LocalDateTime.of(localDate, newtimeSheetItem.from.toLocalTime());
+        newtimeSheetItem.to = LocalDateTime.of(localDate, newtimeSheetItem.to.toLocalTime());
+
+        return newtimeSheetItem;
+
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+}
