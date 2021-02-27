8804a085d5609c3eca2d1730847c33bdcf09bc66
IvanyiA <trachemys13@gmail.com>
2021-02-27 11:07:14 +0100

    Collection.sort(Comparator) Iterator<>.hasnext() next() remove()

--- src/main/java/exam03retake02/BalatonStorm.java
+++ src/main/java/exam03retake02/BalatonStorm.java
@@ -17,32 +17,17 @@ public class BalatonStorm {
         reader.readLine();
         String line;
         while ((line = reader.readLine()) != null) {
-            extractStations(line);
-            extractLevels(line, station);
+            if (line.contains("allomas")) {
+                String[] parts = line.split(":");
+                station = parts[1].substring(2, parts[1].length()-2);
+            }
+            if (line.contains("level\": 3,")) {
+                String[] parts = line.split(":");
+                stationsWithStorm.add(station);
+                stationsWithStorm.sort(Collator.getInstance(new Locale("hu", "HU")));
+            }
         }
         return stationsWithStorm;
-    }
-
-    private void extractStations(String line) {
-        if (line.contains("allomas")) {
-            String[] parts = line.split(":");
-            station = parts[1].substring(2, parts[1].length()-2);
-        }
-    }
-
-    private void extractLevels(String line, String station) {
-        if (line.contains("level")) {
-            String[] parts = line.split(":");
-            int stormLevel = Integer.parseInt(parts[1].replace(",", "").trim());
-            getSortedStationsByLevel(station, stormLevel);
-        }
-    }
-
-    private void getSortedStationsByLevel(String station, int stormLevel) {
-        if (stormLevel >= 3) {
-            stationsWithStorm.add(station);
-            stationsWithStorm.sort(Collator.getInstance(new Locale("hu", "HU")));
-        }
     }
 
 }
