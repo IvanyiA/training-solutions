662ba409423c022e45613c69c806483f8a392b44
IvanyiA <trachemys13@gmail.com>
2021-02-28 14:09:42 +0100

    JDBC lekérdezés és létrehozás Timestamp.valueOf(LocalDateTime) DAO-val

---
+++ src/main/java/activitytracker/ActivityDao.java
@@ -0,0 +1,93 @@
+package activitytracker;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityDao {
+
+    private DataSource dataSource;
+
+    public ActivityDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void insertactivity(Activity activity) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement("insert into activities(start_time, activity_desc, activity_type) values(?,?,?)")
+        ) {
+            statement.setTimestamp(1, Timestamp.valueOf(activity.getStartTime()));
+            statement.setString(2, activity.getDesc());
+            statement.setString(3, activity.getType().toString());
+            statement.executeUpdate();
+
+        } catch (SQLException se) {
+            throw new IllegalArgumentException("Cannot insert", se);
+        }
+    }
+
+
+    public Activity findById(long id) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement("select * from activities where id = ?")
+        ) {
+            statement.setLong(1, id);
+
+            List<Activity> result = selectActivityByPreparedStatement(statement);
+            if (result.size() == 1) {
+                return result.get(0);
+            }
+            throw new IllegalArgumentException("Not found");
+
+        } catch (SQLException se) {
+            throw new IllegalArgumentException("Cannot connect", se);
+        }
+    }
+
+    private List<Activity> selectActivityByPreparedStatement(PreparedStatement statement) {
+        List<Activity> result = new ArrayList<>();
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                Activity activity = new Activity(
+                        resultSet.getLong("id"),
+                        resultSet.getTimestamp("start_time").toLocalDateTime(),
+                        resultSet.getString("activity_desc"),
+                        ActivityType.valueOf(resultSet.getString("activity_type")));
+                result.add(activity);
+            }
+            return result;
+
+        } catch (SQLException se) {
+            throw new IllegalArgumentException("Execute failed", se);
+        }
+    }
+
+    public List<Activity> selectAllActivities() {
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select * from activities")
+        ) {
+            return selectActivityByPreparedStatement(statement);
+
+        } catch (SQLException se) {
+            throw new IllegalStateException("Cannot connect", se);
+        }
+    }
+
+    public List<Activity> selectByType(ActivityType type) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("select * from activities where activity_type =?")
+        ) {
+            statement.setString(1, type.toString());
+            return selectActivityByPreparedStatement(statement);
+        }
+        catch (SQLException se) {
+            throw new IllegalStateException("Cannot connect", se);
+        }
+    }
+
+
+}
