78b7bdd1a819ab9f57f8488a8287c14cab1f65c1
IvanyiA <trachemys13@gmail.com>
2021-03-01 21:12:36 +0100

    TrackPointTest 1. próbálkozás

---
+++ src/test/java/activitytracker/TrackPointTest.java
@@ -0,0 +1,32 @@
+package activitytracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackPointTest {
+
+    @Test
+    void createTrackPoint() {
+        TrackPoint trackPoint = new TrackPoint(
+                1,
+                LocalDateTime.parse("2020-02-01T13:08"),
+                47.5950470, 19.3407574);
+
+        assertEquals(1, trackPoint.getId());
+        assertEquals(LocalDateTime.parse("2020-02-01T13:08"), trackPoint.getTime());
+        assertEquals(47.5950470,trackPoint.getLat());
+        assertEquals(19.3407574,trackPoint.getLon());
+    }
+
+    @Test
+    void createTrackPointError(){
+        assertThrows(IllegalArgumentException.class, ()-> new TrackPoint(
+                1,
+                LocalDateTime.parse("2020-02-01T13:08"),
+                181.5950470, 19.3407574));
+    }
+
+}
