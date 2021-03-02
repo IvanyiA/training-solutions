6dae2d1afcdc80a975266090b18d4eccf44f6da1
IvanyiA <trachemys13@gmail.com>
2021-03-02 13:00:28 +0100

    sql file

--- src/main/resources/db/migration/V2__activities.sql
+++ src/main/resources/db/migration/V2__activities.sql
@@ -5,7 +5,7 @@ activity_desc VARCHAR (255),
 activity_type VARCHAR (255),
 PRIMARY KEY (id));
 
---INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2021-2-12 12:10:11','Biking in Bükk','BIKING');
---INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2021-2-12 12:10:11','Hiking in Bükk','HIKING');
---INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2020-2-11 12:10:11','Biking in Mátra','BIKING');
---INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2011-6-12 12:10:11','Basketball in Park','BASKETBALL');
+INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2021-2-12 12:10:11','Biking in Bükk','BIKING');
+INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2021-2-12 12:10:11','Hiking in Bükk','HIKING');
+INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2020-2-11 12:10:11','Biking in Mátra','BIKING');
+INSERT INTO activities(start_time, activity_desc, activity_type) VALUES('2011-6-12 12:10:11','Basketball in Park','BASKETBALL');
